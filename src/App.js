a567d6a8efb1aa965d6237f9aa06c2ce624713fb
jkunciene <j.kunciene@gmail.com>
2021-01-17 17:18:26 +0200

    Product, Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './app.css'
 import Header from './components/header/Header'
+import Home from './components/home/Home'
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import Product from '../product/Product'
 import './home.css'
 
 function Home() {
     return (
-        <div>
+        <div className='home'>
+            <div className='home_container'>
+                <img className='home_image' 
+                src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg' 
+                alt='amazon image'
+                />
+
+                <div className='home_row'>
+                    <Product
+                        title='Pavadinimas'
+                        price={20}
+                        image='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_1x._SY304_CB431800965_.jpg'
+                        rating={5}
+                    />
+                    <Product/>
+                   
+                </div>
+                  <div className='home_row'>
+                    <Product/>
+                    <Product/>
+                    <Product/>
+
+                </div>
+                  <div className='home_row'>
+                     <Product/>
+                </div>
+                
+            </div>
             
         </div>
     )
